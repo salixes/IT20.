b7a102bd1f3bf31fd3be6dd3ac5e315bd878b7ed
salixes <salizpersonaluse@gmail.com>
2024-09-23 22:52:32 +0800

    it20c

---
+++ src/it20c_pags/IT20C_PAGS.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package it20c_pags;
+
+/**
+ *
+ * @author Hp
+ */
+public class IT20C_PAGS {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
