b1940b759c4f00bc6ce31b78c5291add2b1d8818
salixes <salizpersonaluse@gmail.com>
2024-09-27 23:50:16 +0800

    dev-araylist

---
+++ src/arraylist/ItemManagerclass.java
@@ -0,0 +1,57 @@
+package ARRAYLIST;
+
+import java.util.ArrayList;
+
+public class ItemManagerclass {
+
+    private ArrayList<String> items;
+
+    //Constructor to initialize the ArrayList
+    public ItemManagerclass() {
+        items = new ArrayList<>();
+    }
+
+    //Method to display items
+    public void displayItems() {
+        System.out.println("Items in the list: " + items);
+    }
+
+    //Method to add user's input to arraylist
+    public void addItems(String Hp, String Lenovo, String Acer) {
+        items.add(Hp);
+        items.add(Lenovo);
+        items.add(Acer);
+        System.out.println("You Added: " + items);
+    }
+
+    //Method to update the one item in the list
+    public void updateItem(int index, String newItem) {
+        if (index >= 0 && index < items.size()) {
+            String oldItem = items.set(index, newItem);
+            System.out.println("You updated: " + oldItem + " to " + newItem);
+        } else {
+            System.out.println("Index out of bounds");
+        }
+    }
+
+    //Method to find item in the ArrayList
+    public int findItem(String finditem) {
+        int index = items.indexOf(finditem);
+        if (index != -1) {
+            System.out.println("Found: " + finditem + " at index " + index);
+        } else {
+            System.out.println(finditem + " not found.");
+        }
+        return index;
+    }
+
+    //Method to delete one item in the Arraylist
+    public void deleteItem(int index) {
+        if (index >= 0 && index < items.size()) {
+            String removedItem = items.remove(index);
+            System.out.println("You have successfully deleted the item: " + removedItem);
+        } else {
+            System.out.println("Index out of bounds");
+        }
+    }
+}
