2a6495f8b53958360d7a4f91ce041ad0fe0210ac
salixes <salizpersonaluse@gmail.com>
2024-09-28 15:52:09 +0800

    array

--- src/LO2_SIMPLEIMPLEMENTATION/SIMPLEIMPLEMENTAION.java
+++ src/LO2_SIMPLEIMPLEMENTATION/SIMPLEIMPLEMENTAION.java
@@ -1,30 +1,14 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package LO2_SIMPLEIMPLEMENTATION;
 
-/**
- *
- * @author Hp
- */
 public class SIMPLEIMPLEMENTAION {
-   public static void main(String[] args) {
-      int[] simple_array = {1, 2, 3, 4, 5,6,7,8,9};
-        
-        for (int num : simple_array) {
-            System.out.println(num);
-        }
 
-        int[][] multi_array = {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
+    public static void main(String[] args) {
 
-        // Displaying multi_array elements
-        for (int[] sublist : multi_array) {
-            for (int num : sublist) {
-                System.out.println(num);
-            }
-        }
+        //Declaaring string arrays
+        String[] Names = {"Saliz", "cutie", "Bluesh"};
+
+        //Displaying String array
+        System.out.println(Names.length);
     }
 
 }
