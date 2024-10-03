77c5e3d96f5b8ca77aa7db8f8a8189193208455f
salixes <salizpersonaluse@gmail.com>
2024-10-04 00:46:07 +0800

    linkedlisst

--- src/topic_2_linkedlist_int/Node.java
+++ src/topic_2_linkedlist_int/Node.java
@@ -1,16 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
 package topic_2_linkedlist_int;
 
-import java.util.Scanner;
-
+/**
+ *
+ * @author Hp
+ */
 public class Node {
-
-    int data;
-    Node next;
-//constructor
-
-    Node(int data) {
-        this.data = data;
-        this.next = null;
-
-    }
+    
 }
