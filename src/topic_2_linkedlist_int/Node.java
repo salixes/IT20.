00778ad23d4ded5eb0446067a025cb130cc3e3fc
salixes <salizpersonaluse@gmail.com>
2024-10-03 23:03:20 +0800

    ImportScanner

---
+++ src/topic_2_linkedlist_int/Node.java
@@ -0,0 +1,16 @@
+package topic_2_linkedlist_int;
+
+import java.util.Scanner;
+
+public class Node {
+
+    int data;
+    Node next;
+//constructor
+
+    Node(int data) {
+        this.data = data;
+        this.next = null;
+
+    }
+}
