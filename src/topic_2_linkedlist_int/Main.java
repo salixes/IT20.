45afa49be4a097c71aa46e6ef85c5f4136b7d3cf
salixes <salizpersonaluse@gmail.com>
2024-10-14 14:09:19 +0800

    added stack

--- src/topic_2_linkedlist_int/Main.java
+++ src/topic_2_linkedlist_int/Main.java
@@ -5,6 +5,7 @@ public class Main {
 	public static void main(String[] args) {
 		
 		LinkedList list = new LinkedList();
+                
 
         // Adding elements to the list
         list.add(10);
--- src/topic_2_linkedlist_string/Main.java
+++ src/topic_2_linkedlist_string/Main.java
@@ -1,14 +1,37 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package topic_2_linkedlist_string;
 
-/**
- *
- * @author Hp
- */
 public class Main {
-    
+
+	public static void main(String[] args) {
+		
+		LinkedList list = new LinkedList();
+
+        // Adding elements to the list
+        list.add("Before it sinks in");
+        list.add("Sarah G-Forevers not enough");
+        list.add("Cup of Joe-morena girl");
+        list.add("Jroa-Treat you better");
+        System.out.println("Current Linked List:");
+        System.out.println("------------------------------");
+        list.printList();
+        
+        //Delete
+        list.deleteByValue("The Juans-Hindi tayu pwede");
+        System.out.println();
+        System.out.println("Deleting |The Juans-Hindi tayu pwede| from the list...");
+        System.out.println();
+        System.out.println("Current Linked List:");
+        System.out.println("------------------------------");
+        list.printList();
+        
+        //Move/Swap Pointer
+        System.out.println();
+        System.out.println("Moving/Swapping node from index 1 to index 0");
+        System.out.println();
+        System.out.println("Current Linked List:");
+        System.out.println("------------------------------");
+        list.moveNodePointer(0, 1);
+        list.printList();
+	}
+
 }
