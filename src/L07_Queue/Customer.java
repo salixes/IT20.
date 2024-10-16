922ed51dd4cf2f52cffa5b6c2316f5bc2ec452c5
salixes <salizpersonaluse@gmail.com>
2024-10-16 23:40:01 +0800

    addedqueue

---
+++ src/L07_Queue/Customer.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package L07_Queue;
+
+/**
+ *
+ * @author Saliz II
+ */
+public class Customer {
+    private String name;
+
+    public Customer(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
--- src/Topic3_Stack/Stack.java
+++ src/Topic3_Stack/Stack.java
@@ -3,12 +3,61 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package Topic3_Stack;
+package Topic3_stack;
 
 /**
  *
- * @author Hp
+ * @author Jane Justine
  */
 public class Stack {
+    private int maxSize;
+    private int[] stackArray;
+    private int top;
+
+    // Constructor to initialize the stack with a given size
+    public Stack(int size) {
+        this.maxSize = size;
+        this.stackArray = new int[maxSize];
+        this.top = -1; 
+    }
     
+   // Check if the stack is empty (top == -1) or full (top == maxSize - 1)
+    public boolean isEmpty() {
+        return (top == -1);
+    }
+    
+    public boolean isFull() {
+        return (top == maxSize - 1);
+    }
+
+    public int size() {
+        return top + 1; 
+    }
+    
+    // Pushes a new value onto the stack if it's not full
+    public void push(int value) {
+        if (isFull()) {
+            System.out.println("Stack is full. Cannot push " + value);
+            return;
+        }
+        stackArray[++top] = value;
+    }
+    
+    // Returns the top element of the stack without removing it
+    public int peek() {
+        if (isEmpty()) {
+            System.out.println("Stack is empty. Cannot peek.");
+            return -1;
+        }
+        return stackArray[top];
+    }
+    
+    // Removes and returns the top element of the stack
+    public int pop() {
+        if (isEmpty()) {
+            System.out.println("Stack is empty. Cannot pop.");
+            return -1;
+        }
+        return stackArray[top--];
+    }
 }
